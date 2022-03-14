0d65fad49aac1ca482d438945fc2e86d367af742
pedroserviio <pedroservio2010@gmail.com>
2022-03-14 01:18:02 -0300

    Banco Digital Dio

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import entities.Cliente;
+import entities.Conta;
+import entities.ContaCorrente;
+import entities.ContaPoupanca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
